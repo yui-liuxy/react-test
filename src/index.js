6868909fd33dc83a49f0b77cad8cf4eadba0d426
efyui <Eyui1203@126.com>
2021-06-05 15:24:24 +0800

    ReactBackground

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter as Router} from "react-router-dom";
+import App from "./App";
+import 'antd/dist/antd.css';
+//ant全局化配置
+import { ConfigProvider } from 'antd';
+import zhCN from "antd/lib/locale/zh_CN";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ConfigProvider locale={zhCN}>
+    <Router>
+        <App></App>
+    </Router>
+    </ConfigProvider>,
+    document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
